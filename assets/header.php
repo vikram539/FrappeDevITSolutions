7503fbeea8c8ab588e7ed41449df96303bbfc68f
vikram539 <vikram.engg539@gmail.com>
2022-08-27 21:41:25 +0530

    FrappeDevITSolutions

--- assets/header.php
+++ assets/header.php
@@ -8,8 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='icon' type='image/png' href='<?= $images ?>favicon.png' size='25x25' />
     <title><?= $name; ?></title>
-    <link href="bootstrap5/css/bootstrap.min.css" rel="stylesheet">
-    <script src="bootstrap5/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="<?= $root ?>bootstrap5/css/bootstrap.min.css" rel="stylesheet">
+    <script src="<?= $root ?>bootstrap5/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="<?= $root ?>css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -25,13 +26,7 @@
     <script>
         $(document).ready(function(){
             var path = window.location.href;
-            console.log(path);
-            // if ( path == '' ) {
-            //     path = 'index.php';       
-            // }
-        
-            var target = $(document).find('nav #navbarNav ul li a[href="'+path+'"]').closest("li");
-            // Add active class to target link
+            var target = $(document).find('nav #navbarNav ul li a[href="'+path+'"]').closest("li");            
             target.addClass('active');
         })
     </script>
---
+++ assets/headerAddCart.php
@@ -0,0 +1,24 @@
+<section class='addCart'>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-4 col-lg-2">
+                <div class="headerLogo">
+                    <a class="navbar-brand" href="<?= $root ?>">
+                        <img src="<?= $images ?>logo.png" alt="<?= $name ?>" class="img-fluid" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-8 col-lg-10">
+                <div class="headerRightSection">
+                    <ul>
+                        <li><a href="<?= $root ?>">test</a></li>
+                        <li><a href="<?= $root ?>">test</a></li>
+                        <li><a href="<?= $root ?>">test</a></li>
+                        <li><a href="<?= $root ?>">test</a></li>
+                        <li><a href="<?= $root ?>">test</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
