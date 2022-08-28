becf4d4da8962009820d4be7dcd44c9a81a05f84
vikram539 <vikram.engg539@gmail.com>
2022-08-28 17:01:29 +0530

    Top header desktop complete

--- assets/header.php
+++ assets/header.php
@@ -12,9 +12,6 @@
     <link href="<?= $root ?>bootstrap5/css/bootstrap.min.css" rel="stylesheet">
     <script src="<?= $root ?>bootstrap5/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="<?= $root ?>css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     <!-- Owl Stylesheets -->
     <link rel="stylesheet" href="<?= $root ?>owlcarousel/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?= $root ?>owlcarousel/css/owl.theme.default.min.css">
--- assets/headerAddCart.php
+++ assets/headerAddCart.php
@@ -1,21 +1,58 @@
 <section class='addCart'>
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-4 col-lg-2">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-2 col-lg-2">
                 <div class="headerLogo">
-                    <a class="navbar-brand" href="<?= $root ?>">
+                    <a href="<?= $root ?>">
                         <img src="<?= $images ?>logo.png" alt="<?= $name ?>" class="img-fluid" />
+                        <p>
+                            <span>delicacy</span>
+                            <span>organic</span> 
+                        </p>
                     </a>
                 </div>
             </div>
-            <div class="col-8 col-lg-10">
+            <div class="col-5 col-lg-7">
+                <div class="headerMiddleSection">
+                    <ul>
+                        <li>
+                            <p>delivery to</p>
+                        </li>
+                        <li>
+                            <p class='address'>
+                                <i class="fas fa-map-marker-alt"></i>                                
+                                <span>Address line1, address line2</span>
+                            </p>
+                        </li>
+                        <li>
+                            <form method="post" class='searchForm'>
+                                <input type="text" class="form-control searchInput" placeholder="What are you looking for?" />
+                                <i class="fas fa-search"></i>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-5 col-lg-3">
                 <div class="headerRightSection">
                     <ul>
-                        <li><a href="<?= $root ?>">test</a></li>
-                        <li><a href="<?= $root ?>">test</a></li>
-                        <li><a href="<?= $root ?>">test</a></li>
-                        <li><a href="<?= $root ?>">test</a></li>
-                        <li><a href="<?= $root ?>">test</a></li>
+                        <li class='favorite'>
+                            <p>
+                                <i class="far fa-heart"></i>
+                            </p>
+                        </li>
+                        <li class='myCart'>
+                            <p>
+                                <span>My Cart <lable>$255.00</label></span>  
+                                <i class="fas fa-chevron-down"></i>                              
+                            </p>
+                        </li>
+                        <li class='userProfile'>
+                            <p> 
+                                <i class="far fa-user-circle"></i>  
+                                <span>User Name <i class="fas fa-chevron-down"></i></span>                             
+                            </p>
+                        </li>
                     </ul>
                 </div>
             </div>
